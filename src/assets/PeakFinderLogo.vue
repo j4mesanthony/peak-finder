b4ca380d527329e5ae43d42d065dad0c5c64b6ed
James Anthony <janthony@greenleaf.com.au>
2023-01-04 20:05:15 +1000

    Bottom navigation styling.

--- src/assets/PeakFinderLogo.vue
+++ src/assets/PeakFinderLogo.vue
@@ -1,8 +1,12 @@
 <template>
-  <span class="material-icons" style="font-size: 42px">landscape</span>
+  <span class="material-icons" style="font-size: 70px">landscape</span>
   <h1>Peak Finder</h1>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 32px;
+}
+</style>
